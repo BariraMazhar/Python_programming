7b365b5a2641883d82af9f520ccdc0ed7f8c17ab
Barira Mazhar <bmmcpk@gmail.com>
2023-10-19 10:46:29 -0700

    midified simple python program

--- 01_simple_program.py
+++ 01_simple_program.py
@@ -1,7 +1,9 @@
+#simple print statement
+print("Hello world")
+#declaring variables
 a=5    #integer
 b=10   #integer
 c=10.5 #float
 d=20   #integer
 sum = a+b+c
 print("Sum is",sum)
-print("Hello world")
