518053fb4ce3ee796b6f20a289a53a771d70d4f9
BariraMazhar <bmmcpk@gmail.com>
2023-10-22 09:10:24 -0700

    Operators in python

---
+++ Ex1_Calculator_in _python.py
@@ -0,0 +1,12 @@
+a=10
+b=5
+#Addition
+print("The value of ",a,"+",b,"is",a+b)
+#subtraction
+print("The value of ",a,"-",b,"is",a-b)
+#Multiplication
+print("The value of ",a,"*",b,"is",a*b)
+#Divison
+print("The value of ",a,"/",b,"is",a/b)
+#Modulus
+print("The value of ",a,"%",b,"is",a%b)
