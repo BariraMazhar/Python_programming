6d14aba0794ddebcb88740b1d03fe131d41c5a28
BariraMazhar <bmmcpk@gmail.com>
2023-10-22 08:44:37 -0700

    Variables and data type

---
+++ 02_variable_and_type_function.py
@@ -0,0 +1,9 @@
+a=1         #integer
+b="python"  #string
+c=10.5      #float
+d=True      #boolean
+print(a)   #to print value of a
+print(b)   #to print value of b
+print(type(b))   #to print type of b
+print(type(c))   #to print type of c 
+print(type(d))   #to print type of d
