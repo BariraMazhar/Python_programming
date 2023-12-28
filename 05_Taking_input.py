c56604047b60d381682757b1e0b2c0fcd1f14db8
BariraMazhar <bmmcpk@gmail.com>
2023-12-28 05:16:17 -0800

    Taking User input

---
+++ 05_Taking_input.py
@@ -0,0 +1,10 @@
+name=input("Enter your name: ")
+print("My name is ",name)
+
+
+a=input("Enter first number: ")
+b=input("Enter second number: ")
+print("Sum of two numbers is ",int(a)+int(b))
+print("Subtraction of two numbers is ",int(a)-int(b))
+print("Product of two numbers is ",int(a)*int(b))
+print("Divison of two numbers is ",int(a)/int(b))
