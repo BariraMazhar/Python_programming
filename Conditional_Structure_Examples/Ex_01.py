616b713003b5ac28060027cb3edd85f46a1d5752
Barira-Mazhar <bmmcpk@gmail.com>
2025-07-09 04:02:46 -0700

    Added first example of conditional structure

---
+++ Conditional_Structure_Examples/Ex_01.py
@@ -0,0 +1,10 @@
+#   Q#01
+age = int(input("Enter your age: "))
+if(age < 13):
+    print("child")
+elif(age < 20):
+    print("teenager")
+elif(age < 60):
+    print("adult")
+else:
+    print("Senior")
