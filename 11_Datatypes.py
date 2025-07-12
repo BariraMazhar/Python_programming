89285fa1833e6751f8561dbbeaf854a2be71c65a
Barira-Mazhar <bmmcpk@gmail.com>
2025-07-12 04:31:29 -0700

    Datatypes in python

---
+++ 11_Datatypes.py
@@ -0,0 +1,46 @@
+#numbers
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#String
+print("Hello")
+print('Hello')
+
+#Boolean
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+
+#Lists
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+print(thislist[1])
+
+#tuples
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+thistuple[2] = "mango"
+print(thistuple)
+
+
+#dictionary
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+print(thisdict["brand"])
+
+
+#sets
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+
