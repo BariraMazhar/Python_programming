6fdae80f52a0853e1546a66f6c5cf5114451719a
Barira-Mazhar <bmmcpk@gmail.com>
2025-07-14 04:52:46 -0700

    Numbers.py

---
+++ 12_Numbers.py
@@ -0,0 +1,63 @@
+x = 10
+y = 5
+
+#arithmetic operations
+sum = x + y
+sub = x - y
+mul = x * y
+div = x / y
+mod = x % y
+
+print(sum)
+print(sub)
+print(mul)
+print(div)
+print(mod)
+
+
+
+
+
+#power of -> **
+power = y**x        # y raised to the power x
+print(power)
+
+
+# use of comparison and logical operators
+z = 15
+print(x, y, z)
+
+print (x > y > z)
+
+print(x < y and y < z)
+
+print(x < y or y < z)
+
+
+
+#using math library
+import math
+
+print(math.floor(4.5))
+print(math.floor(-6.8))
+
+print(math.ceil(3.2))
+print(math.ceil(-7.2))
+
+
+#complex numbers
+print(2 + 1j * 3)     #3 is multiplied to complex part
+print((2 + 1j) * 3)     #3 is multiplied to whole equation
+
+
+#octal numbers
+print(0o20)
+print(oct(20))
+
+#Hexadecimal numbers
+print(0xFF)
+print(hex(44))
+
+#Binary numbers
+print(bin(64))
+
