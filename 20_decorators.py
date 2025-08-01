f1b6ab8c40e749e60fe39cce2375c020b8a451ac
Barira-Mazhar <bmmcpk@gmail.com>
2025-08-01 18:45:39 +0500

    added probelm 2 in decorators

--- 20_decorators.py
+++ 20_decorators.py
@@ -8,7 +8,7 @@ def timer(func):
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
-        print(f"{func, __name__} ran in {end-start} time")
+        print(f"{func. __name__} ran in {end-start} time")
         return result
     return wrapper
 
@@ -22,5 +22,24 @@ def example_function(n):
 example_function(7)
 
 
+#Problem 2:
+        # Create a decorator to print the function name and the values of the arguments every time the funtion is called.
+#solution:
+
+def debug(func):
+    def wrapper(*args, **kwargs):
+        args_value = ', '.join(str(arg) for arg in args)
+        kwargs_value = ', '.join( f"{k}, {v}" for k, v in kwargs.items())
+        print(f" calling: {func. __name__} with args {args_value} and kwargs {kwargs_value}")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@debug
+def greet(name, greeting = "Hi" ):
+    print(f"{greeting}, {name} ")
+    
+
+greet('Bera', 'Asslam o alaikum')
 
 
